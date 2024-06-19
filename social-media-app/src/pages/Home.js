f81385604cfbc5dc1af492eb369d3071c73c70be
Pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-06-19 15:01:34 +0530

    create rightbar component

--- social-media-app/src/pages/Home.js
+++ social-media-app/src/pages/Home.js
@@ -1,9 +1,15 @@
+import Post from "../components/Post/Post";
+import Rightbar from "../components/Rightbar/Rightbar";
+import Sidebar from "../components/Sidebar/Sidebar";
 import Topbar from "../components/Topbar/Topbar";
 
 const Home = () => {
   return (
     <div>
       <Topbar />
+      <Sidebar />
+      <Post />
+      <Rightbar />
     </div>
   );
 };
