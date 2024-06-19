281255048e0fe5808d81461f561aaeb7de9ac993
Pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-06-19 14:10:08 +0530

    create assets/person folder

--- social-media-app/src/components/Topbar/Topbar.js
+++ social-media-app/src/components/Topbar/Topbar.js
@@ -1,12 +1,47 @@
-import "./topbar.css";
-import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import "./Topbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
-export default function Topbar() {
+function Topbar() {
   return (
     <div className="topbarContainer">
-      <div className="topbarLeft"></div>
-      <div className="topbarCenter"></div>
-      <div className="topbarRight"></div>
+      <div className="topbarLeft">
+        <span className="logo">Nicesocial </span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+        </div>
+        <img src="assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
     </div>
   );
 }
+
+export default Topbar;
